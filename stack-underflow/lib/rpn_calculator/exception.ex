a0bf5e0d0b0dd4cf9922788997c0355de0235468
Boris Potapov <borispotapov@iMac-Anuta.local>
2024-06-20 15:27:48 -0700

    solutions

--- stack-underflow/lib/rpn_calculator/exception.ex
+++ stack-underflow/lib/rpn_calculator/exception.ex
@@ -5,4 +5,18 @@ defmodule RPNCalculator.Exception do
   end
 
   # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    defexception message: "stack underflow occurred"
+
+    def exception(value) do
+      case value do
+        [] -> %StackUnderflowError{}
+        _ -> %StackUnderflowError{message: "stack underflow occurred, context: " <> value}
+      end
+    end
+  end
+
+  def divide(stack) when length(stack) < 2, do: raise(StackUnderflowError, "when dividing")
+  def divide([0 | _]), do: raise(DivisionByZeroError)
+  def divide([x, y | _]), do: y / x
 end
